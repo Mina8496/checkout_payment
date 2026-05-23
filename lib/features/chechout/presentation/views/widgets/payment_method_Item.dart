6fa1bb75edc5ced9445fa695f10775b39ffe9600
Mina Kamil <mena.m4077@gmail.com>
2026-05-23 14:25:46 +0300

    - add PaymentMethodsListView - add PaymentMethodsBottomSheet - refactor PaymentMethodItem

--- lib/features/chechout/presentation/views/widgets/payment_method_Item.dart
+++ lib/features/chechout/presentation/views/widgets/payment_method_Item.dart
@@ -1,43 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class PaymentMethodItem extends StatelessWidget {
+  const PaymentMethodItem({
+    super.key,
+    required this.isActive,
+    required this.image,
+  });
+
   final bool isActive;
-
-  const PaymentMethodItem({super.key, this.isActive = false});
-
+  final String image;
   @override
   Widget build(BuildContext context) {
     return AnimatedContainer(
-      duration: Duration(milliseconds: 600),
+      duration: const Duration(milliseconds: 300),
       width: 103,
       height: 62,
       decoration: ShapeDecoration(
         shape: RoundedRectangleBorder(
           side: BorderSide(
             width: 1.50,
-            color: isActive ? Colors.green : Colors.grey,
+            color: isActive ? const Color(0xFF34A853) : Colors.grey,
           ),
           borderRadius: BorderRadius.circular(15),
         ),
         shadows: [
           BoxShadow(
-            color: isActive ? Colors.green : Colors.grey,
+            color: isActive ? const Color(0xFF34A853) : Colors.white,
             blurRadius: 4,
-            offset: Offset(0, 0),
+            offset: const Offset(0, 0),
             spreadRadius: 0,
-          ),
+          )
         ],
       ),
-
       child: Container(
         decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(15),
-          color: Colors.white,
-        ),
+            borderRadius: BorderRadius.circular(15), color: Colors.white),
         child: Center(
-          child: Image.asset(
-            'assets/images/SVGRepo_iconCarrier.png',
-            height: 24,
+          child: SvgPicture.asset(
+            image,
           ),
         ),
       ),
