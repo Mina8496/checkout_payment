39096c261a8e7a644764bbdcdf860befbf2311ab
Mina Kamil <mena.m4077@gmail.com>
2026-05-24 14:16:16 +0300

    refactor CustomButton for CustomButtonBlocConsumer

---
+++ lib/features/chechout/presentation/views/widgets/custom_button_bloc_consumer.dart
@@ -0,0 +1,36 @@
+import 'package:checkout_payment/core/utils/styles.dart';
+import 'package:flutter/material.dart';
+
+class CustomButtonBlocConsumer extends StatelessWidget {
+  final String text;
+  final void Function()? onTap;
+  final bool isLoading;
+  const CustomButtonBlocConsumer({
+    super.key,
+    required this.onTap,
+    required this.text,
+    this.isLoading = false,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        width: double.infinity,
+        height: 73,
+        decoration: ShapeDecoration(
+          color: Color(0xff34A853),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadiusGeometry.circular(15),
+          ),
+        ),
+        child: Center(
+          child: isLoading
+              ? CircularProgressIndicator()
+              : Text(text, textAlign: TextAlign.center, style: Styles.style22),
+        ),
+      ),
+    );
+  }
+}
